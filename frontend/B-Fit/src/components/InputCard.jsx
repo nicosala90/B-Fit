f14f71baba968ca5ff37cff1850685309f980d84
Zsolt Levai <zsoltlieber@gmail.com>
2023-03-29 16:17:55 +0200

    add CSS to BMI page

--- frontend/B-Fit/src/components/InputCard.jsx
+++ frontend/B-Fit/src/components/InputCard.jsx
@@ -37,23 +37,25 @@ function InputCard() {
     }
     //check user input validation
     function submitUserData(user) {
-        console.log(user);
-        fetch('/api/user/actual-user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-        setUser({
-            "gender": "",
-            "age": "",
-            "weight": "",
-            "height": ""
-        });
-        setShow(true);
-    }
+        if (user["gender"] !== "" && user["age"] !== "" && user["weight"] !== "" && user["height"] !== "") {
+            fetch('/api/user/actual-user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+            setUser({
+                "gender": "",
+                "age": 0,
+                "weight": 0,
+                "height": 0
+            });
+            setShow(true);
+        }
+        else console.log(user);
 
+    }
     return (
         <div className="input-table">
             <div className="input-fields">
@@ -65,9 +67,11 @@ function InputCard() {
                 <input placeholder="age" type="number" value={user.age} onChange={handleAgeChange}></input>
                 <input placeholder="weight" type="number" value={user.weight} onChange={handleWeightChange}></input>
                 <input placeholder="height" type="number" value={user.height} onChange={handleHeightChange}></input>
-                <button onClick={() => { submitUserData(user); }}>Submit</button>
+                <button id="submitBtn" onClick={() => { submitUserData(user); }}>Submit</button>
+                <div className="BMI-value">
+                    <BMIValue show={show} setShow={setShow} />
+                </div>
             </div>
-            <BMIValue show={show} setShow={setShow}/>
         </div>
     )
 }
