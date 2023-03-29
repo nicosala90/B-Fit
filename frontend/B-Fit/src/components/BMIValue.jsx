c0a2e058d19ed9938696c00ac772c3c19fc20428
nicosala90 <nicosala90@gmail.com>
2023-03-29 16:34:52 +0200

    BMI value refactored to 2 decimal points

--- frontend/B-Fit/src/components/BMIValue.jsx
+++ frontend/B-Fit/src/components/BMIValue.jsx
@@ -5,8 +5,15 @@ function BMIValue({ value }) {
 
     return (
         <div className="BMI-value">
-            <p>Your calculated Body Mass Index is:</p>
-            {value}
+            {value.toFixed(2) > 0 ? 
+                <>
+                    <p>Your calculated Body Mass Index is:</p>
+                    <>
+                        {value.toFixed(2)}
+                    </>
+                </>
+             : null
+            }
         </div>
     )
 }
