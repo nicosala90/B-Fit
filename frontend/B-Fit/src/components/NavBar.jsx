36cd16f97126eb160d07881bd3a8c3b91b2b8683
nicosala90 <nicosala90@gmail.com>
2023-03-30 14:20:36 +0200

    create NavBar component with logo

---
+++ frontend/B-Fit/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+
+function NavBar(){
+
+    return (
+        <div className="nav-bar">
+            <img className="logo" src="/logo-text.png" alt="B-Fit" width="100" height="100"></img>
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Contact</h1>
+        </div>
+    )
+}
+export default NavBar;
