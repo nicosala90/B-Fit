e81df5c9c82e7e76f6e7230b5a9818dc341d8b26
nicosala90 <nicosala90@gmail.com>
2023-03-30 14:27:18 +0200

    create Footer component

--- frontend/B-Fit/src/App.jsx
+++ frontend/B-Fit/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import InputCard from './components/InputCard'
 import NavBar from './components/NavBar'
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <NavBar/>
      <InputCard/>
+     <Footer/>
     </div>
   )
 }
