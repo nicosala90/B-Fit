10358fc88020ae1eb7b7489636758984b102287e
nicosala90 <nicosala90@gmail.com>
2023-04-24 14:37:32 +0200

    create dropdown menu for signup and login

--- frontend/B-Fit/src/components/NavBar.jsx
+++ frontend/B-Fit/src/components/NavBar.jsx
@@ -8,13 +8,20 @@ function NavBar() {
                 <img className="logo" src="/logo-text.png" alt="B-Fit" width="100" height="100"></img>
             </div>
             <div className="nav-options">
-              
+            <div class="dropdown">
+                    <button class="dropbtn">Sign in
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-content">
+                    <Link className="link-component" to="/login">Login</Link>
+                    <Link className="link-component" to="/signin">Sign In</Link>
+                    </div>
+                </div>
                 <Link className="link-component" to="/">Home</Link>
-                <Link className="link-component" to="/signin">Sign In</Link>
                 <Link className="link-component" to="/calculator">BMI Calculator</Link>
                 <Link className="link-component" to="/progress">Your Progress</Link>
                 <Link className="link-component" to="/contact">Contact</Link>
-                <Link className="link-component" to="/login">Login</Link>
+                
             </div>
         </div>
     )
